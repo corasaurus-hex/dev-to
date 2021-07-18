232b3c5e62297773d8b29e7f1b01d4506d39d7ee
Cora Sutton <cora@sutton.me>
2021-07-17 22:19:35 -0500

    Pull all the posts-tests-finding code into a ns

---
+++ src/posts.clj
@@ -0,0 +1,31 @@
+(ns posts
+  (:require [babashka.fs :as fs]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+(defn bb-edn-files
+  [root-path]
+  (map str (fs/glob root-path "*/bb.edn")))
+
+(defn read-edn-file
+  [path]
+  (with-open [in (java.io.PushbackReader. (io/reader path))]
+    (->> in
+         (partial edn/read {:eof ::EOF})
+         repeatedly
+         (take-while (partial not= ::EOF))
+         first)))
+
+(defn has-test-task?
+  [bb-edn-path]
+  (-> bb-edn-path
+      read-edn-file
+      :tasks
+      (get 'test)))
+
+(defn paths-to-posts-with-tests
+  [root-path]
+  (->> root-path
+       bb-edn-files
+       (filter has-test-task?)
+       (map (comp str fs/parent))))
